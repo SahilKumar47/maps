18a8f7b4de665a96872e66594e2c95218cd5c2ac
Sahil Kumar <49636096+SahilKumar47@users.noreply.github.com>
2020-12-25 22:20:38 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import * as am4core from "@amcharts/amcharts4/core";
 import * as am4maps from "@amcharts/amcharts4/maps";
 import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
-
+ 
 am4core.useTheme(am4themes_animated);
 
 class App extends Component {
